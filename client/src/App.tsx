13e4beb750ca9d1fce66bf39883d4dbdb6d7b421
eualexdev <eujosealexsandro@gmail.com>
2022-03-26 00:25:03 -0300

    Comecei a fazer a tela de login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Index from "./pages"
+import Login from "./pages/login"
 import SplashScreen from "./pages/splashScreen"
 
 
 const App = () => {
-  const [loading,setLoading] = useState<boolean>(true)
+  const [loading,setLoading] = useState<boolean>(false)
+  useEffect(() => setLoading(true),[])
+  const login = localStorage.getItem("loginExist")
+
+  function RouteLoged(){
+    if (login === "exist"){
+      return (
+        <Routes>
+          <Route path="/" element={ <Index/>} />
+        </Routes>
+      )
+    } else {
+      return (
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )
+    }
+  }
 
   function ConfigRoutes(){
     return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={
-            <Index/>
-          } />
-        </Routes>
+          <RouteLoged />
       </BrowserRouter>
     )
   }
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,65 @@
-import { Fragment } from "react"
+import { Fragment,useState } from "react"
+import "../styles/login.scss"
 
-const Login = () => {
+function Register(){
+  return <></>
+}
+
+function Login(){
+  const [typeInput,setTypeInput] = useState("password")
+  const [icon,setIcon] = useState("fas fa-eye-slash")
+
+  function setVisiblePasswordFunc(){
+    if (typeInput == "password"){
+      setIcon("fas fa-eye")
+      setTypeInput("text")
+    } else {
+      setIcon("fas fa-eye-slash")
+      setTypeInput("password")
+    }
+  }
+
+  // PROFONT AWESOME v5.10 USADA
+  // NÃO PODE SER OUTRA VERSÂO 
+  // ACHA UMA SOLUÇÃO MELHOR
   return (
-    <Fragment>
-      Faça Login
+    <Fragment>  
+      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+      <div className="login-container">
+        <section className="login-name">
+          <h1>
+            Faça Login
+          </h1>
+        </section>
+        <section className="login-components">
+          <input type="email" placeholder="Email" className="login-input" />
+          <div className="login-input-component">
+            <input type={typeInput} placeholder="Senha" className="login-input login-input-password"/>
+            <section onClick={setVisiblePasswordFunc} className="login-icon-div">
+              <i className={icon}></i>
+            </section>
+          </div>
+          <button className="login-button">
+            Login
+          </button>
+        </section>
+      </div>
     </Fragment>
   )
 }
+
+const LoginRegister = () => {
+  const [type,setType] = useState<number>(0)
+
+  function Render(){
+    if (type == 0){
+      return <Login />
+    } else { 
+      return <Register />
+    }
+  }
+
+  return <Render />
+}
+
+export default LoginRegister
