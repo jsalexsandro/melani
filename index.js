795a1e2f5d3a13648579786a9574d89e2753021f
José Alexsandro <68931503+jsalexsandro@users.noreply.github.com>
2022-03-27 14:28:43 -0300

    mudei o index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { routes } = require("./router")
 
 const app     = express()
 const server  = http.createServer(app)
-const PORT    = process.env.PORT || 80
+const PORT    = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
